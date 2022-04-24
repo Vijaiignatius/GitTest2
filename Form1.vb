bce464e66dd0b1b772a9f39cefee506c3deed46f
Home <Home@LAPTOP-RGOF0A4S>
2022-04-24 21:30:32 +0530

    efer

--- Form1.vb
+++ Form1.vb
@@ -2,4 +2,8 @@
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         MessageBox.Show("Vijai")
     End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        MessageBox.Show("Vijai1")
+    End Sub
 End Class
